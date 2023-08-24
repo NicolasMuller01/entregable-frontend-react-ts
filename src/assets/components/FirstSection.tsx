070dc4b85256f4a41faa5ce287af41f635f0eb91
Nicolas Müller <nicolasmatiasmuller19@gmail.com>
2023-08-24 14:50:38 -0300

    form changes and first section fixed

--- src/assets/components/FirstSection.tsx
+++ src/assets/components/FirstSection.tsx
@@ -4,7 +4,7 @@ import "../Styles/principalSection.css"
 export const FirstSection = () => {
   return (
     <section id='about' className='max-w-7xl container mx-auto px-10 mb-20 flex flex-col md:px-15 md:flex-row md:align-middle place-items-center justify-start'>
-        <div className='box-left-side flex flex-col mx-auto lg:place-items-start lg:justify-center'>
+        <div className='box-left-side flex flex-col mx-auto lg:mx-0 lg:place-items-start lg:justify-center'>
             <h1 className='text-4xl m-0 lg:text-5xl'>Want anything to be <br></br> easy with <span>LaslesVPN.</span></h1>
             <p className='left-side-p mt-5'>Provide a network for all your needs with ease and fun using <b>LaslesVPN</b> <br></br>discover interesting features from us.</p>
             <button onClick={() => window.open("https://www.google.com/search?q=perritos&tbm=isch&source=lnms&sa=X&ved=2ahUKEwjQjPaO9c2AAxVBqZUCHbMwAvMQ0pQJegQIDRAB&biw=1536&bih=695&dpr=1.25", "_blank")} className='left-side-button mt-10 w-64 h-14 bg-red-500 rounded-lg drop-shadow-xl'>Get Started</button>
--- src/assets/components/SeventhSectionForm.tsx
+++ src/assets/components/SeventhSectionForm.tsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
 
 type FormType = {
   fullName: string;
@@ -7,63 +9,48 @@ type FormType = {
   message: string;
 };
 
-
+const schema = yup
+  .object({
+    fullName: yup.string()
+    .required("Please enter your full name")
+    .min(5, "This field must have at least 5 characters")
+    .max(30, "This field must have a maximum of 30 characters"),
+    phone: yup.string().
+    required("Please enter your phone number")
+    .min(9, "your number must have at least 9 characters").
+    max(15, "your number must have a maximum of 30 characters"),
+    email: yup.string().
+    required("Please enter your email").
+    email(),
+    message: yup
+    .string()
+    .required("Please enter your message")
+    .min(10, "This message must have at least 10 characters")
+    .max(200, "this message must have a maximum of 200 characters")
+  })
+  .required(); 
 
 export const SeventhSectionForm = () => {
 
-  const [formData, setFormData] = useState<FormType>({
-    fullName: '',
-    email: '',
-    phone: '',
-    message: '',
-  });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormType>({
+    resolver: yupResolver(schema)
+  })
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = event.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
+  const onSubmit = handleSubmit((data) => {
+    fetch('https://6xrb5goi1l.execute-api.us-east-1.amazonaws.com/api/send-email', {
+      method: "POST",
+              headers: {
+                  "Content-Type": "application/json",
+              },
+              body: JSON.stringify(data)
+    })
 
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault(); 
-
-    if (checkForm()){
-
-      fetch('https://6xrb5goi1l.execute-api.us-east-1.amazonaws.com/api/send-email', {
-        method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(formData)
-      })
-
-      setFormData({
-        fullName: '',
-        email: '',
-        phone: '',
-        message: '',
-      })
-      
-      alert('Form Sent Correctly, We will contact you soon...')
-    }else{
-      alert('Some field is wrong, please try again')
-    }
-
-
-      };
-
-      const checkForm = (): boolean =>{
-        if(formData.fullName === ''){
-          return false;
-        }else if(!/\S+@\S+\.\S+/.test(formData.email)){
-          return false;
-        }else if(formData.phone === ''){
-          return false
-        }
-        return true
-      }
+    alert('Email sent successfully, we will contact you soon...')
+  })
 
   return (
     <section id='help' className='pb-20'>
@@ -71,41 +58,47 @@ export const SeventhSectionForm = () => {
         <h3 className="text-center text-slate-900 text-4xl font-medium leading-10 mb-5">Let's Talk</h3>
       </div>
       <div className="form-box flex justify-center flex-col items-center align-middle">
-        <form className="flex flex-col justify-center items-center" onSubmit={handleSubmit}>
+
+        <form className="flex flex-col justify-center items-center" onSubmit={onSubmit}>
+
           <input
-            onChange={handleInputChange}
-            value={formData.fullName}
-            className="sm:w-96 w-64 h-16 mb-5 drop-shadow-lg mx-auto"
+            className="sm:w-96 w-64 h-16 mt-5 drop-shadow-lg mx-auto"
             type="text"
+            {...register("fullName")}
             name="fullName"
-            placeholder="Name"
+            placeholder="Full name"
           />
+          {errors.fullName?.message ? <p className="h-3 text-red-500">{errors.fullName?.message}</p> : <p className="h-3"></p>}
+
           <input
-            onChange={handleInputChange}
-            value={formData.email}
-            className="sm:w-96 w-64 h-16 mb-5 drop-shadow-lg"
+            className="sm:w-96 w-64 h-16 mt-5 drop-shadow-lg"
             type="email"
+            {...register("email")}
             name="email"
             placeholder="Email"
           />
-           <input
-            onChange={handleInputChange}
-            value={formData.phone}
-            className="sm:w-96 w-64 h-16 mb-5 drop-shadow-lg"
+          {errors.email?.message ? <p className="h-3 text-red-500">{errors.email?.message}</p> : <p className="h-3"></p>}
+
+          <input
+            className="sm:w-96 w-64 h-16 mt-5 drop-shadow-lg"
             type="tel"
+            {...register("phone")}
             name="phone"
             placeholder="Phone"
           />
+          {errors.phone?.message ? <p className="h-3 text-red-500">{errors.phone?.message}</p> : <p className="h-3"></p>}
+
           <textarea
-            onChange={handleInputChange}
-            value={formData.message}
-            className="w-64 sm:w-96 mb-5 resize-none drop-shadow-lg"
+            className="w-64 sm:w-96 mt-5 resize-none drop-shadow-lg"
             rows={10}
             cols={50}
+            {...register("message")}
             name="message"
             placeholder="Write something."
           ></textarea>
-          <button className="w-44 h-11 bg-red-500 rounded-3xl text-white text-base font-bold drop-shadow-xl mx-auto " type="submit">
+          {errors.message?.message ? <p className="h-3 text-red-500">{errors.message?.message}</p> : <p className="h-3"></p>}
+
+          <button className="w-44 h-11 bg-red-500 mt-5 rounded-3xl text-white text-base font-bold drop-shadow-xl mx-auto " type="submit">
             Send
           </button>
         </form>
